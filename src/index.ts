2df4fb3e833c60a262510ab6021483fd8b023913
Pedro Neto <ntbraga@live.com>
2019-11-05 12:34:51 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export { IUser as User, UserDefinition, encodePassword } from './models/user.mod
 export { IPeople as People, PeopleDefinition } from './models/people.model';
 export { IAddress as Address, AddressDefinition } from './models/address.model';
 export { baseModel as baseModelPlugin, DefaultSchemaOptions } from './lib';
+
+export interface LoginRequest {
+    user: string;
+    password: string;
+}
